1ad198e448cdd3faa2262267e4a1be2b0b03a84d
Ash <ashley@juice49.com>
2021-05-21 23:47:29 +0100

    Log matches

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import { nodeFileTrace } from '@vercel/nft'
 
 const { fileList } = await nodeFileTrace(['./playwright.js'])
 const matches = fileList.filter(name => name.includes('browsers.json'))
+console.log(matches)
 assert.equal(matches.length, 1)
