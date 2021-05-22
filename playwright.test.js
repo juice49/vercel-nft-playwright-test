7a45aa931deb876ec72df2dacf46c37103dd77cc
Ash <ashley@juice49.com>
2021-05-22 02:02:22 +0100

    Add info

--- playwright.test.js
+++ playwright.test.js
@@ -10,6 +10,8 @@ const testNft = async nft => {
   )
 }
 
+// Run both @vercel/nft 0.11.2 and 0.12.0, and check that the file
+// `node_modules/playwright-core/browsers.json` is traced.
 t.test('NFT includes `playwright-core/browsers.json`', async () => {
   const [matches_0_11_2, matches_0_12_0] = await Promise.all([
     testNft(nodeFileTrace_0_11_2),
